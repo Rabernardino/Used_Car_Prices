94fb1814097ec59d00bf49ae4af6f1f9691b5262
rabernardino <rabernardino@gmail.com>
2024-05-28 10:11:49 -0300

    Adding exploratory and processed notebooks

--- packages/statistics_func.py
+++ packages/statistics_func.py
@@ -1,17 +1,37 @@
 
 
-
+import string
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def histogram_chart(x_var, y_var_label, data_src):
-
+def plot_chart(chart_type, x_var, y_var = None, data_src = None, title = None):
+    
     plt.subplots(figsize = (12,6))
     sns.set_style('whitegrid')
 
-    fig = sns.histplot(x= x_var, data= data_src)
-    fig.set_title(f'Distribution of - {data_src[x_var].name.capitalize()}', fontsize=16)
-    fig.set_ylabel(f'{y_var_label.capitalize()}', fontsize=14)
-    fig.set_xlabel(f'{data_src[x_var].name.capitalize()}')
+    if x_var != None:
+        x_label = string.capwords(x_var.replace('_', ' '))
+    
+    else:
+        x_label = ''
+    
+    if y_var != None:
+        y_label = string.capwords(y_var.replace('_', ' '))
 
-    return
+    else:
+        y_label = ''
+
+
+    if chart_type == 'histogram':
+
+        fig = sns.histplot(x = x_var, data = data_src)
+        fig.set_title(f'Histogram - {title}', fontsize = 16)
+
+    elif chart_type == 'boxplot':
+
+        fig = sns.boxplot(x = x_var, y = y_var, data = data_src)
+        fig.set_title(f'Boxplot - {title}', fontsize = 16)
+
+    
+    fig.set_xlabel(x_label, fontsize = 14)
+    fig.set_ylabel(y_label, fontsize = 14)
---
+++ src/train.py
@@ -0,0 +1,61 @@
+
+
+# Importing libraries
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.simplefilter('ignore')
+
+from sklearn.dummy import DummyClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
+
+file_path = os.path.dirname('__file__')
+
+def train_comparative(df):
+    
+    
+    """" Getting the features and the target variable from the dataframe """
+    x_variable = df.iloc[:,0:-1]
+    y_variabel = df.iloc[:,-1:]
+
+    
+    """" Split the data into train and test subsets """
+    x_train, x_test, y_train, y_test = train_test_split(x_variable, y_variabel, test_size=0.25)
+
+
+    """" Initializing the three different models used on the comparative """
+    dummy_model_stratify = DummyClassifier(strategy='stratified').fit(x_train,y_train)
+    dummy_model_stratify_accuracy = (dummy_model_stratify.score(x_train,y_train)*100).round(2)
+
+
+    dummy_model_most_freq = DummyClassifier(strategy='most_frequent').fit(x_train,y_train)
+    dummy_model_most_freq_accuracy = (dummy_model_most_freq.score(x_train,y_train)*100).round(2)
+
+
+    linear_svc = LinearSVC().fit(x_train, y_train)
+    linear_svc_accuracy = (linear_svc.score(x_train, y_train)*100).round(2)
+
+
+    decision_tree_model = DecisionTreeClassifier(max_depth=3).fit(x_train, y_train)
+    decision_tree_model_accuracy = (decision_tree_model.score(x_train, y_train)*100).round(2)
+
+    """" Return a dataframe comparing the four models accuracy """
+    accuracy_df_comparative = pd.DataFrame([[dummy_model_stratify_accuracy],[dummy_model_most_freq_accuracy],[linear_svc_accuracy],[decision_tree_model_accuracy]], index = ['Dummy_Stratify', 'Dummy_Most_Freq','LinearSVC','DecisionTreeClassifier'], columns=['Model Accuracy'])
+    
+    return accuracy_df_comparative
+
+def main():
+    df = pd.read_csv(os.path.join(file_path, os.pardir, 'data/processed/processed.csv'))
+
+    model_comparative = train_comparative(df)
+    print(model_comparative)
+
+if __name__ == '__main__':
+    main()
